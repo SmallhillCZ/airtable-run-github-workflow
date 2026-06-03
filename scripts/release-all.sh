b98c0a55052ac144647c1bdb1a0ef9c4c3f9ab38
Martin Kopeček <martin.kopecek@smallhill.cz>
2026-06-03 13:45:04 +0000

    feat: update packages and release script, add bo data collection remote

--- scripts/release-all.sh
+++ scripts/release-all.sh
@@ -1,4 +1,8 @@
 
-block release --remote scoring
-block release --remote data-collection
-block release --remote index-website
+#!/usr/bin/env bash
+trap 'exit 130' INT
+for f in .block/*.remote.json; do
+  name="$(basename "$f" .remote.json)"
+  echo -e "\n\033[33m⚡ Releasing $name...\033[0m"
+  block release --remote "$name"
+done
